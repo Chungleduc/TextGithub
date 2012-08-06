62c9b86cf5d19fca8ec90af3e483ca4538bef52b
chungleduc <chungleduc@gmail.com>
2012-08-06 14:55:02 +0700

    first upload

---
+++ Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 453px; width: 907px;">
+    
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="CustomerID"></asp:Label>
+        <asp:TextBox ID="CustomerID" runat="server" style="margin-left: 49px"></asp:TextBox>
+        <asp:Label ID="Label7" runat="server" Text="Region"></asp:Label>
+        <asp:TextBox ID="Region" runat="server" style="margin-left: 78px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="CompanyName"></asp:Label>
+        <asp:TextBox ID="CompanyName" runat="server" style="margin-left: 28px"></asp:TextBox>
+        <asp:Label ID="Label8" runat="server" Text="PostalCode"></asp:Label>
+        <asp:TextBox ID="PostalCode" runat="server" style="margin-left: 49px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="ContactName"></asp:Label>
+        <asp:TextBox ID="ContactName" runat="server" style="margin-left: 39px"></asp:TextBox>
+        <asp:Label ID="Label9" runat="server" Text="Country"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Country" runat="server" style="margin-left: 49px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="ContactTitle"></asp:Label>
+        <asp:TextBox ID="ContactTitle" runat="server" style="margin-left: 50px"></asp:TextBox>
+        <asp:Label ID="Label10" runat="server" Text="Phone"></asp:Label>
+        <asp:TextBox ID="Phone" runat="server" style="margin-left: 81px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Address"></asp:Label>
+        <asp:TextBox ID="Address" runat="server" style="margin-left: 72px"></asp:TextBox>
+        <asp:Label ID="Label11" runat="server" Text="Fax"></asp:Label>
+        <asp:TextBox ID="Fax" runat="server" style="margin-left: 97px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="City"></asp:Label>
+        <asp:TextBox ID="City" runat="server" style="margin-left: 97px"></asp:TextBox>
+        <br />
+&nbsp;<br />
+    
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Them " />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Sua" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="Xoa" />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="True" 
+            CellPadding="4" ForeColor="#333333" GridLines="None" Width="514px">
+            <RowStyle BackColor="#EFF3FB" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+            <AlternatingRowStyle BackColor="White" />
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Dim objConn As SqlConnection
+    Dim objCmd As SqlCommand
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+       
+        Dim strConnString As String
+        strConnString = "Data Source=.\SQLEXPRESS;AttachDbFilename=D:\project\WebSite3\App_Data\NORTHWND.MDF;Integrated Security=True;Connect Timeout=30;User Instance=True"
+        objConn = New SqlConnection(strConnString)
+        objConn.Open()
+        BindData()
+    End Sub
+    Sub BindData()
+        Dim strSQL As String
+        strSQL = "SELECT * FROM Customers"
+
+        Dim dtReader As SqlDataReader
+        objCmd = New SqlCommand(strSQL, objConn)
+        dtReader = objCmd.ExecuteReader()
+        GridView1.DataSource = dtReader
+        GridView1.DataBind()
+
+        dtReader.Close()
+        dtReader = Nothing
+
+
+    End Sub
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Ci As String = CustomerID.Text
+        Dim Cn As String = CompanyName.Text
+        Dim Ct As String = ContactName.Text
+        Dim Ctt As String = ContactTitle.Text
+        Dim Ad As String = Address.Text
+        Dim Cty As String = City.Text
+        Dim Rg As String = Region.Text
+        Dim Pc As String = PostalCode.Text
+        Dim Ctry As String = Country.Text
+        Dim Ph As String = Phone.Text
+        Dim Fa As String = Fax.Text
+        Dim sql As String = "insert into Customers values('" & Ci & "','" & Cn & "','" & Ct & "','" & Ctt & "','" & Ad & "','" & Cty & "','" & Rg & "','" & Pc & "','" & Ctry & "','" & Ph & "','" & Fa & "')"
+        objCmd = New SqlCommand(sql, objConn)
+        Try
+            objCmd.ExecuteNonQuery()
+        Catch ex As Exception
+            Response.Write("<H2>Thêm không thành công</H2>")
+        End Try
+        Page_Load(sender, e)
+    End Sub
+
+    Protected Sub Button2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim Ci As String = CustomerID.Text
+        Dim Cn As String = CompanyName.Text
+        Dim Ct As String = ContactName.Text
+        Dim Ctt As String = ContactTitle.Text
+        Dim Ad As String = Address.Text
+        Dim Cty As String = City.Text
+        Dim Rg As String = Region.Text
+        Dim Pc As String = PostalCode.Text
+        Dim Ctry As String = Country.Text
+        Dim Ph As String = Phone.Text
+        Dim Fa As String = Fax.Text
+        Dim sql As String = "update Customers set CompanyName='" & Cn & "',ContactName='" & Ct & "',ContactTitle='" & Ctt & "',Address='" & Ad & "',City='" & Cty & "',Region='" & Rg & "',PostalCode='" & Pc & "',Country='" & Ctry & "',Phone='" & Ph & "',Fax='" & Fa & "' where CustomerID='" & Ci & "'"
+        objCmd = New SqlCommand(sql, objConn)
+        Try
+            objCmd.ExecuteNonQuery()
+        Catch ex As Exception
+            Response.Write("<H2>Sửa không thành công</H2>")
+        End Try
+        Page_Load(sender, e)
+    End Sub
+
+    Protected Sub GridView1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles GridView1.SelectedIndexChanged
+        Dim Ci As String = GridView1.SelectedRow.Cells(1).Text
+        Dim Cn As String = GridView1.SelectedRow.Cells(2).Text
+        Dim Ct As String = GridView1.SelectedRow.Cells(3).Text
+        Dim Ctt As String = GridView1.SelectedRow.Cells(4).Text
+        Dim Ad As String = GridView1.SelectedRow.Cells(5).Text
+        Dim Cty As String = GridView1.SelectedRow.Cells(6).Text
+        Dim Rg As String = GridView1.SelectedRow.Cells(7).Text
+        Dim Pc As String = GridView1.SelectedRow.Cells(8).Text
+        Dim Ctry As String = GridView1.SelectedRow.Cells(9).Text
+        Dim Ph As String = GridView1.SelectedRow.Cells(10).Text
+        Dim Fa As String = GridView1.SelectedRow.Cells(11).Text
+        CustomerID.Text = Ci
+        CompanyName.Text = Cn
+        ContactName.Text = Ct
+        ContactTitle.Text = Ctt
+        Address.Text = Ad
+        City.Text = Cty
+        Region.Text = Rg
+        PostalCode.Text = Pc
+        Country.Text = Ctry
+        Phone.Text = Ph
+        Fax.Text = Fa
+    End Sub
+
+    Protected Sub Button3_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim sql As String = "delete from Customers where CustomerID='" & CustomerID.Text & "'"
+        objCmd = New SqlCommand(sql, objConn)
+        Try
+            objCmd.ExecuteNonQuery()
+        Catch ex As Exception
+            Response.Write("<H2>Xóa không thành công</H2>")
+        End Try
+        Page_Load(sender, e)
+    End Sub
+End Class
